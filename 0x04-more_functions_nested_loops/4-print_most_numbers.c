aea37e9699153b64c0c2d2159b21fea4871a8ca6
timothymaza <theodoremaza18@gmail.com>
2022-09-18 08:26:28 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 {
 	int number;
 
-	for (number = '0'; number <= '9'; number++)
+	for (number = 0; number <= 9; number++)
 	{
-		if (number == '2' || number == '4');
+		if (number == 2 || number == 4);
 	}
 	else
 	{
