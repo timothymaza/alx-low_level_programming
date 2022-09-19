33c0ada4e51e0d6a555c66e9cf866704d28d5b97
timothymaza <theodoremaza18@gmail.com>
2022-09-19 10:10:52 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * Reset_to_98 - resetting the value to 98
+ * @n: checked
+ * Return : Always 0
+ */
+
+void reset_to_98(int *n);
+{
+	int *ptr;
+
+		*ptr = n;
+
+		*ptr = 98;
+}
