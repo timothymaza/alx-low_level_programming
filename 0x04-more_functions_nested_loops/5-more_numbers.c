315ee917679835c2a6a0d3c582c7f8cbfdbf5caf
timothymaza <theodoremaza18@gmail.com>
2022-09-19 07:00:56 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,6 +14,8 @@ void more_numbers(void)
 
 	for (row = 1; row <= 10; row++)
 	{
+		for (number = 0; number <= 14; number++)
+		{
 		tens = number / 10;
 		units = number % 10;
 		if (number > 9)
@@ -21,6 +23,7 @@ void more_numbers(void)
 			_putchar(tens + '0');
 		}
 		_putchar(units + '0');
-	}
+		}
 	_putchar('\n');
+	}
 }
