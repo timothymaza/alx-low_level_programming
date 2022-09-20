c10c77e0961deadb5e0d25de9d01a2e2f006b7fd
timothymaza <theodoremaza18@gmail.com>
2022-09-19 17:43:13 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * puts_2 - prints every character of a string
+ *
+ * @str: string input value
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int a;
+	for (a = 0; str[a] != '\0'; ++a)
+	{
+		if (a % 2 == 0)
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
